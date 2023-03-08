d951eaa45ee4df94db12cafba86f80f941dae053
50l3r <500l3r@gmail.com>
2023-03-08 22:45:17 +0100

    Add publish to npm workflow

--- src/layouts/main/partials/layout/footer.vue
+++ src/layouts/main/partials/layout/footer.vue
@@ -1,9 +1,9 @@
 <template>
     <div id="k-footer" class="p-4 dark:bg-gray-800 bg-white hidden sm:flex">
         <div
-            class="flex flex-col md:flex-row items-center justify-between text-sm w-full"
+            class="flex flex-col md:flex-row items-center justify-between text-sm w-full text-gray-800 dark:text-gray-200"
         >
-            <div class="text-gray-800 dark:text-gray-200 order-2 md:order-1">
+            <div class="order-2 md:order-1">
                 <span class="font-bold mr-1">&copy; 2023 - </span>
                 <a
                     :href="copyright.link"
@@ -16,18 +16,14 @@
 
             <ul class="font-semibold order-1 flex flex-row gap-4 select-none">
                 <li class="whitespace-nowrap">
-                    <a class="cursor-pointer" @click="supportVisible = true">
+                    <a
+                        class="cursor-pointer"
+                        href="https://calltek.es/soporte"
+                        target="_blank"
+                    >
                         <k-icon icon="life-ring" class="mr-1" />
                         Soporte
                     </a>
-
-                    <k-drawer
-                        v-model="supportVisible"
-                        placement="right"
-                        title="Soporte"
-                    >
-                        DRAWER ON RIGHT
-                    </k-drawer>
                 </li>
 
                 <li class="whitespace-nowrap">
@@ -53,17 +49,14 @@
 </template>
 
 <script lang="ts">
-    import { defineComponent, ref } from 'vue'
+    import { defineComponent } from 'vue'
     import { changelogEnabled, copyright } from '@/helpers/config'
 
     export default defineComponent({
         name: 'KTFooter',
         setup() {
-            const supportVisible = ref(false)
-
             return {
                 changelogEnabled,
-                supportVisible,
                 copyright
             }
         }
