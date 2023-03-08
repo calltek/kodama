6ef64e2e88f862f94d8896d71b55896dcdcbb5a7
50l3r <500l3r@gmail.com>
2023-03-08 20:39:40 +0000

    Deploying to main from  @ 16aa3488c1a38002bea1ea887e074d02aebc929b 🚀

---
+++ docs/assets/403.story.ce3a0331.js
@@ -0,0 +1,16 @@
+var i=Object.defineProperty;var a=(t,e)=>i(t,"name",{value:e,configurable:!0});import"./jsx-runtime.50e3ea3d.js";import{aL as r,aM as d,aO as l,aQ as y}from"./Props.83de5850.js";import"./iframe.38129425.js";import{m as f,e as x}from"./app.a51160cf.js";import"./string.afc03a9e.js";import"./es.map.constructor.249e4cc2.js";import"./es.number.to-fixed.73993e76.js";import"./vue.esm-bundler.7a829ca1.js";import"./query.2abf0db7.js";import"./_plugin-vue_export-helper.cdc0426e.js";function p(){return p=Object.assign?Object.assign.bind():function(t){for(var e=1;e<arguments.length;e++){var n=arguments[e];for(var s in n)Object.prototype.hasOwnProperty.call(n,s)&&(t[s]=n[s])}return t},p.apply(this,arguments)}a(p,"_extends");const m=a(t=>({components:{App:f},setup(){return x().replace({name:"403"}),{args:t}},template:"<app />"}),"Template"),S={Template:m},g="wrapper";function u({components:t,...e}){return r(g,p({},S,e,{components:t,mdxType:"MDXLayout"}),r(y,{title:"Layouts/403",decorators:[()=>({template:'<div id="app" class="fixed h-screen w-screen top-0 left-0"><story /></div>'})],mdxType:"Meta"}),r(l,{name:"Default",mdxType:"Story"},m.bind({})))}a(u,"MDXContent");u.isMDXComponent=!0;const c=m.bind({});c.storyName="Default";c.parameters={storySource:{source:`args => ({
+  components: {
+    App
+  },
+  setup() {
+    const router = useRouter();
+    router.replace({
+      name: '403'
+    });
+    return {
+      args
+    };
+  },
+  template: '<app />'
+})`}};const o={title:"Layouts/403",decorators:[()=>({template:'<div id="app" class="fixed h-screen w-screen top-0 left-0"><story /></div>'})],includeStories:["defaultStory"]},M={Default:"defaultStory"};o.parameters=o.parameters||{};o.parameters.docs={...o.parameters.docs||{},page:()=>r(d,{mdxStoryNameToKey:M,mdxComponentAnnotations:o},r(u,null))};const j=["Template","defaultStory"];export{m as Template,j as __namedExportsOrder,o as default,c as defaultStory};
+//# sourceMappingURL=403.story.ce3a0331.js.map
---
+++ docs/assets/utils.afff62ba.js
@@ -0,0 +1,2 @@
+var o=Object.defineProperty;var r=(a,n)=>o(a,"name",{value:n,configurable:!0});const e=r(()=>(Date.now()+Math.random()).toString(36).replace(".",""),"uid"),s=["white","black","gray","primary","secondary","success","warning","danger","info"];export{s as c,e as u};
+//# sourceMappingURL=utils.afff62ba.js.map
