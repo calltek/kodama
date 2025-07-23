1469a5bd62a00968651ddfc132a0ff367a4d9abd
felipegcx <ggfelipegc@gmail.com>
2025-07-23 08:41:02 -0500

    feat: add new menu mode

--- src/layouts/main/partials/aside/aside.vue
+++ src/layouts/main/partials/aside/aside.vue
@@ -4,29 +4,38 @@
             class="px-3 pb-5 pt-5 flex relative"
             :class="collapsed ? 'justify-center' : 'justify-between'"
         >
+            <div
+                v-if="isMobile && menuType === 'simple'"
+                class="flex items-center cursor-pointer"
+                @click="handlerColapsed(true)"
+            >
+                <k-icon icon="xmark" type="far" class="text-lg mx-2" />
+                <k-title :size="5">MENU</k-title>
+            </div>
+
             <k-logo
-                v-if="!collapsed"
+                v-else-if="!collapsed"
                 dark
                 side
                 class="cursor-pointer max-w-[14rem]"
-                @click="collapsed = false"
+                @click="handlerColapsed(true)"
             />
             <k-logo
                 v-else
                 dark
                 :side="false"
                 class="cursor-pointer max-w-[54px]"
-                @click="collapsed = false"
+                @click="handlerColapsed(false)"
             />
 
             <k-button
-                v-if="!collapsed"
+                v-if="(isMobile || menuType === 'default') && !collapsed"
                 icon="chevrons-left"
                 link
                 color="primary"
                 class="-mr-2"
-                @click="collapsed = true"
-            ></k-button>
+                @click="handlerColapsed(true)"
+            />
         </div>
         <div class="overflow-y-auto py-5 px-3 h-full flex-1">
             <k-menu :collapsed="collapsed" />
@@ -39,8 +48,15 @@
 
 <script lang="ts">
     import { useConfig } from '@/store'
-    import { computed, defineComponent, resolveDynamicComponent } from 'vue'
+    import {
+        computed,
+        defineComponent,
+        inject,
+        reactive,
+        resolveDynamicComponent
+    } from 'vue'
     import KMenu from './menu.vue'
+    import { Config } from '@/config/_settings'
 
     export default defineComponent({
         name: 'KAside',
@@ -61,8 +77,39 @@
                     config.set('aside.collapsed', value)
                 }
             })
+            const isMobile = computed(() => {
+                const config = useConfig()
+                return config.get('isMobileView') || false
+            })
 
-            return { collapsed, isComponent }
+            const settings = reactive(inject('$settings') as Config)
+
+            const menuType = computed(() => {
+                return settings?.menuType || 'default'
+            })
+
+            const handlerColapsed = (value?: boolean) => {
+                if (
+                    (!collapsed.value &&
+                        menuType.value === 'block' &&
+                        !isMobile.value) ||
+                    (!collapsed.value &&
+                        menuType.value === 'simple' &&
+                        !isMobile.value)
+                ) {
+                    collapsed.value = false
+                } else {
+                    collapsed.value = value ?? !collapsed.value
+                }
+            }
+
+            return {
+                collapsed,
+                menuType,
+                isMobile,
+                isComponent,
+                handlerColapsed
+            }
         }
     })
 </script>
